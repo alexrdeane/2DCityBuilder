6cf5b1463b2263dbc41d548dfda3445ee787b809
Alex Deane <alexrdeane@hotmail.com>
2019-09-23 11:33:54 +1000

    resources script

---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resources : MonoBehaviour
+{
+    public int money = 0;
+    public int hourMoney = 15;
+    public int buildingPower;
+    public float timer = 0f;
+    public int population;
+    public float overallHappiness = 0f;
+    public int builders;
+    public bool building;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        HourlyCurrency();
+    }
+
+    void HourlyCurrency()
+    {
+        if (timer >= 10)
+        {
+            money += hourMoney;
+            Debug.Log(money);
+            timer = 0;
+        }
+    }
+}
