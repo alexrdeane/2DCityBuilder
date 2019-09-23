8775f12cbcb47d8fc7d19134665f79f8b2d2ea93
Sinclair <sinfram248@gmail.com>
2019-09-23 11:56:07 +1000

    Push

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
+	GameManager gameManager;
+
+	void Start() {
+		if (!gameManager) gameManager = FindObjectOfType<GameManager>();
+	}
+	void OnMouseDown() {
+		Debug.Log("A");
+		gameManager.Place(transform.position);
+	}
 	void Update() {
 
 	}
